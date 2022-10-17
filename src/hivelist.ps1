77f3ec5393e112c61c9a57cb2eb7294d048b68d9
gregzakh <grishanz@yandex.com>
2022-10-18 01:06:54 +0500

    A  src/hivelist.ps1

---
+++ src/hivelist.ps1
@@ -0,0 +1,4 @@
+# registry hives list
+($x = Get-Item HKLM:\SYSTEM\CurrentControlSet\Control\hivelist).Property.ForEach{
+  ($_ -split '(\\.+)\\')[1] # becase first is an empty value
+} | Sort-Object -Unique
