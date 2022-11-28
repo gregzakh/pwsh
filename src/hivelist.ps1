178a9c95b84c496a28143a7902874e7901a96284
gregzakh <grishanz@yandex.com>
2022-11-28 23:10:09 +0500

    M src/hivelist.ps1

--- src/hivelist.ps1
+++ src/hivelist.ps1
@@ -1,6 +1,6 @@
 # registry hives list
 if ($IsWindows) {
   ($x = Get-Item HKLM:\SYSTEM\CurrentControlSet\Control\hivelist).Property.ForEach{
-    ($_ -split '(\\.+)\\')[1] # becase first is an empty value
+    ($_ -split '(\\.+)\\')[1] # because first is an empty value
   } | Sort-Object -Unique
 }
